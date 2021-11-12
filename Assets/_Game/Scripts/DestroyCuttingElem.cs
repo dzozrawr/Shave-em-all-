7d567c6a27497b594e672035b6cf4494d00c1a59
dzozrawr <jovan_96@live.ru>
2021-11-12 09:15:14 +0100

    organized project structure and added wool deletion for optimization

--- Assets/_Game/Scripts/DestroyCuttingElem.cs
+++ Assets/_Game/Scripts/DestroyCuttingElem.cs
@@ -2,26 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyWool : MonoBehaviour
+public class DestroyCuttingElem : MonoBehaviour
 {
-    private bool isToBeDestroyed = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isToBeDestroyed)
-        {
-
-        }
+        
     }
 
-    public void startDestroyTimer() //moze da se poboljsa tako sto se brise kad je van pogleda kamere
+    void OnBecameInvisible()
     {
-        
+        Destroy(gameObject);
     }
 }
